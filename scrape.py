e5c04edd89e68c60eafd8259e23ab123b670f28f
Rishit Bansal <rishit.bansal0@gmail.com>
2015-11-15 12:13:27 +0530

    Development Process: 1. Seperated into functions instead of single class. 2. Made parse function recognize tab indentations and DOM Selectors. 3. Made classes for URL, and parser Objects, with appropriate methods. 4. Handles URL, and parsers in eval. 5. Added try except clauses to raise errors.

--- scrape.py
+++ scrape.py
@@ -1,18 +1,83 @@
 Symbol = str          # A Scheme Symbol is implemented as a Python str
 List   = list         # A Scheme List is implemented as a Python list
 Number = (int, float) # A Scheme Number is implemented as a Python int or float\
-class ScrapeIt:
-	def __init__(self,cfile):
-		print self.parseit(open(cfile))
-	def tokenize(self,char):
-		return char.split()
-	def parseit(self,code):
-		return [[self.atom(token) for token in self.tokenize(line)] for line in code.readlines()]
-	def atom(self,token):
-	    "Numbers become numbers; every other token is a symbol."
-	    try: return int(token)
+Env = dict
+
+class DOMParser:
+	def __init__(self,parse,parent):
+		self.parser = parse
+		if parent.__class__.__name__ == "URL":
+			self.parent = parent
+		else:
+			raise ValueError("Parent of Parser Object should be valid URL.")
+	def parse(self):
+		from bs4 import BeautifulSoup
+		soup = BeautifulSoup(self.parent.getsource())
+		return soup.select(self.parser)
+class URL:
+	def __init__(self,url):
+		self.url = url
+		self.source = ""
+	def getsource(self):
+		import requests
+		if self.source == "":
+			self.source = requests.get(self.url).text.encode('utf-8')
+		return self.source
+
+def run(cfile):
+	for line in open(cfile).readlines():
+		print parse(line)
+		print eval(parse(line))
+
+def tokenize(char):
+	return char.split()
+
+def parse(code):
+	i = 0
+	if code[0] == "\t":
+		while code[i] == "\t":
+			i += 1
+	j = i
+	if code[j] == "(":
+		try:
+			while code[j] != ")":
+				j += 1
+		except IndexError:
+			raise SyntaxError(") expected after (")
+		return [i,code[i:j+1]]	
+	if len(tokenize(code)) == 1:
+		return [i,tokenize(code)[0]]
+	else:
+		return [i,tokenize(code)]
+
+def atom(token):
+	"Numbers become numbers; every other token is a symbol."
+	try: return int(token)
+	except ValueError:
+	    try: return float(token)
 	    except ValueError:
-	        try: return float(token)
-	        except ValueError:
-	            return Symbol(token)
-ScrapeIt("pokemonscraper.scrape")
+	        return Symbol(token)
+
+def environment():
+	envir = Env()
+	envir.update({
+		"levels":{}
+		})
+	return envir
+
+global_env = environment()
+
+def eval(x,env=global_env):
+	if x[1][:4] == "http":
+		env["levels"].update({x[0]:URL(x[1])})
+	elif x[1][0] == "(":
+		try: 
+			x[1] = x[1].replace("(","")
+			x[1] = x[1].replace(")","")
+			env["levels"].update({x[0]:DOMParser(x[1],env["levels"][x[0]-1])})
+		except KeyError:
+			raise ValueError("Parser expects URL declaration.")
+	elif x[1][0] == "store":
+		return "Variable Definition"
+
+run("pokemonscraper.scrape")
